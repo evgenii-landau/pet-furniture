413d2f9826a05c67320f26fea601d3469f07f04c
Evgenii Kotenev <koteniov.zgenya@mail.ru>
2024-04-26 16:07:37 +0300

    added pagination functional

--- furniture/goods/views.py
+++ furniture/goods/views.py
@@ -1,15 +1,21 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_list_or_404, render
 from goods.models import Products
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug, page=1):
     if category_slug == 'vse-tovary':
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+        
+    paginator = Paginator(goods, 3)
+    current_page = paginator.page(page)
+    
     context = {
         "title": "Home - Каталог",
-        "goods": goods,
+        "goods": current_page,
+        "cat_slug": category_slug,
     }
     return render(request, "goods/catalog.html", context=context)
 
