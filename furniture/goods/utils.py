f9395595cb5437c5dfb4d8e9244574c118442d70
Evgenii Kotenev <koteniov.zgenya@mail.ru>
2024-04-29 19:21:33 +0300

    complited text search

--- furniture/goods/utils.py
+++ furniture/goods/utils.py
@@ -1,6 +1,11 @@
 from django.db.models import Q
 from goods.models import Products
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+    SearchHeadline,
+)
 
 
 def q_search(query):
@@ -10,8 +15,30 @@ def q_search(query):
     vector = SearchVector("name", "description")
     query = SearchQuery(query)
 
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+    result = (
+        Products.objects.annotate(rank=SearchRank(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
 
+    result = result.annotate(
+        headline=SearchHeadline(
+            "name",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
+    result = result.annotate(
+        bodyline=SearchHeadline(
+            "description",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
+    
+    return result
     # keywords = [word for word in query.split() if word not in "кнасотнадподподоизбезувдляооколообза"]
 
     # q_objects = Q()
