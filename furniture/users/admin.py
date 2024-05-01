e0e97ade095445b1d1b36bf88118bb9517b0c189
Evgenii Kotenev <koteniov.zgenya@mail.ru>
2024-05-01 13:08:32 +0300

    login form and controller are ready

--- furniture/users/admin.py
+++ furniture/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from users.models import User
 
-# Register your models here.
+
+
+admin.site.register(User)
+
+# @admin.register(User)
+# class UsersAdmin(admin.ModelAdmin):
+#     prepopulated_fields = {"slug": ("name",)}
--- furniture/users/views.py
+++ furniture/users/views.py
@@ -1,16 +1,34 @@
-from calendar import c
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from users.forms import UserLoginForm
 
 
 def login(request):
-    context = {"title": "Авторизация"}
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse("main:index"))
+    else:
+        form = UserLoginForm()
+
+    context = {
+        "title": "Home - Авторизация",
+        "form": form
+    }
 
     return render(request, "users/login.html", context=context)
 
 
 def registration(request):
     context = {
-        "titlte": "Регистрация",
+        "title": "Home - Регистрация",
     }
 
     return render(request, "users/registration.html", context=context)
@@ -18,7 +36,7 @@ def registration(request):
 
 def profile(request):
     context = {
-        "titlte": "Профиль",
+        "title": "Home - Профиль",
     }
 
     return render(request, "users/profile.html", context=context)
